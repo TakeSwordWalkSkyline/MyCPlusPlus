a3fba66a49c9cea18707f3fa74c4b6b9687898d2
TakeSwordWalkSkyline <wangxinbzsc@gmail.com>
2021-03-29 10:34:53 +0800

    fix quick sort

--- sorting/MyQuickSort.cpp
+++ sorting/MyQuickSort.cpp
@@ -4,19 +4,21 @@
 int partition(int *a, int low, int high) {
     int pivot = a[low]; //基准数
     int i = low;
-    int j = high - 1;
+    int j = high;
     // int pos = 0;
     while(i < j) {
         //下面这种实现方式移动i和j的指针,由于两个while都有进行判断,最终i和j一定会相等,不会出现i>j的情况
         while (i < j && a[j] >= pivot) j--; // 如果找到比pivot小的数则跳出循环,此时j的位置确定
-        while (i < j && a[i] < pivot) i++; //如果找到比pivot大的数则跳出循环,此时i的位置确定
+        while (i < j && a[i] <= pivot) i++; //如果找到比pivot大的数则跳出循环,此时i的位置确定
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
-    int temp = a[i];
-    a[i] = a[low];
-    a[low] = temp;
+    // int temp = a[i];
+    // a[i] = a[low];
+    // a[low] = temp;
+    a[low] = a[i];
+    a[i] = pivot;
     for (int i = 0; i < high; i++) {
         printf("%d", a[i]);
     }
@@ -47,7 +49,7 @@ int main () {
         std::cout << "\n";
         std::cin >> arr[i];
     }
-    quickSort(arr, 0, size);
+    quickSort(arr, 0, size - 1);
     std::cout << "Sorted array\n";
     for (int i = 0; i < size; ++i) {
         std::cout << arr[i] << " ";
